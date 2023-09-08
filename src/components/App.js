2d66e576cf69632729a92b4390692683d8b57cea
Imane Belaid <belaidimane@gmail.com>
2023-09-08 23:55:07 +0100

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -14,9 +14,18 @@ function Home() {
       <h1>Home</h1>
     </div>
   );
-}
+};
 
 {/* write an <About> component here */}
+function About(){
+  return(
+    <div id="about">
+      <h1>
+        About
+      </h1>
+    </div>
+  )
+}
 
 
 function App() {
@@ -24,7 +33,9 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
